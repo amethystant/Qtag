518587fdfa8c869619d8b689b77360e66fb83e24
Karel Patlejch <kaja.patlejch@gmail.com>
2015-02-26 12:29:47 +0100

    Added some comments

--- audiofile.h
+++ audiofile.h
@@ -55,25 +55,41 @@ public:
     TagEditorLayout* getEditorLayout();
 
 private:
+
+    /*
+    Pointers to all possible tags, the value is NULL if the
+    file does not have the tag (e.g. a MPEG file has only id3v1 and id3v2)
+    */
     TagLib::Ogg::XiphComment *xiphComment;
     TagLib::RIFF::Info::Tag *infoTag;
     TagLib::ID3v2::Tag *id3v2;
     TagLib::ID3v1::Tag *id3v1;
     TagLib::ASF::Tag *asfTag;
     TagLib::APE::Tag *apeTag;
+    
+     // the full path to the file
     QString path;
+   
+      //this booleans indicate if the file has the tag. default value is false
     bool hasFileXiphComment;
     bool hasFileId3v1;
     bool hasFileId3v2;
     bool hasFileApeTag;
     bool hasFileAsfTag;
     bool hasFileInfoTag;
+    
     AudioFormat format;
+    
+    //Some basic info
     QString name;
     int track;
     QString album;
     QString artist;
+    
+    //The layout of the tag editors 
     TagEditorLayout* editorLayout;
+    
+    //Pointer to the main window
     MainWindow* window;
 
 public slots:
