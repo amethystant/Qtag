e38751a113c94a979878d28fc26617eaee8f98f2
Karel Patlejch <kaja.patlejch@gmail.com>
2015-05-05 20:12:56 +0200

    Added a window icon

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 int main(int argc, char *argv[]) {
 
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/images/Qtag.png"));
     QCoreApplication::setApplicationName("Qtag");
     QCoreApplication::setOrganizationName("Qtag");
     QApplication::setStyle(getStyleFromSettings());
